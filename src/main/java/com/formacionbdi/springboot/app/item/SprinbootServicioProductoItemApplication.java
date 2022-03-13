ef15eb9654af8d336e74bf4629be07195bc687b7
Diego de la Cruz <diego.dlacruz@gmail.com>
2022-03-12 18:45:14 -0600

    Initial commit

---
+++ src/main/java/com/formacionbdi/springboot/app/item/SprinbootServicioProductoItemApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootServicioProductoItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootServicioProductoItemApplication.class, args);
+	}
+
+}
